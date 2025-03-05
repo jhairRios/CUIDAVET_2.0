a330578e90b06a13b84018d5cf4d63174e5359ac
EDSON JHAIR RIOS COTO <ejrios@uth.hn>
2025-03-04 23:55:42 -0600

    Agregar uso de Auth en ClientesController

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
 use App\Models\Moneda;
+use Illuminate\Support\Facades\Auth;
 
 class ClientesController extends Controller
 {
